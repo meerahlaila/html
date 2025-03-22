0f0c6593418f5633b9829bc4d98711dee9604586
meerahlaila <edunami2k19@gmail.com>
2025-03-22 21:29:59 +0600

    Intro to JS

---
+++ Javascript 1/main.js
@@ -0,0 +1,13 @@
+document.getElementById("myDiv").innerHTML = "<h1>Hello<h1/>"
+document.getElementById("h4").innerHTML = "<h3>Hello<h3/>"
+
+var a = 10; 
+let b= 20;
+let c= a+b
+let r = 6%5;
+a += 2;
+
+
+
+document.writeln("The sum is: " ,c, "<br>")
+document.writeln(a)
